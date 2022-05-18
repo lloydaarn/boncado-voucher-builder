633cb287a0aafcc90903e22b3f7722795b07ae08
lloydaarn <lloydaaron.barnacha@yahoo.com>
2022-05-18 14:21:44 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <BuilderStage></BuilderStage>
+</template>
+
+<script>
+import BuilderStage from './components/BuilderStage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BuilderStage
+}
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/BuilderStage.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="builder-stage-container">
+
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped lang="scss">
+#builder-stage-container {
+    position: relative;
+    width: 700px;
+    height: 500px;
+    margin: auto;
+    border: 1px solid  #ddd;
+}
+</style>
