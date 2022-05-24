ce20b54cb055f4cfdf09b89fa4f8d4f1232d86e7
lloydaarn <lloydaaron.barnacha@yahoo.com>
2022-05-24 14:26:32 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,3 +7,12 @@ import './registerServiceWorker'
 import store from './store'
 
 createApp(App).use(store).mount('#app')
+
+window.addEventListener('beforeinstallprompt', (event) => {
+    // Prevent the mini-infobar from appearing on mobile.
+    event.preventDefault();
+    alert('👍', 'beforeinstallprompt', event);
+    // Stash the event so it can be triggered later.
+    window.deferredPrompt = event;
+
+  });
